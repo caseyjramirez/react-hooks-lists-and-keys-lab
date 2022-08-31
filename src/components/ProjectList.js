6472fbafba205ebf6ff610f097e44e11841f6f36
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-31 11:02:01 -0500

    commit tree 3b7244d15feccf43254816b3cac8b8afa74b5f1b

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,17 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(project => {
+          const {name, about, technologies} = project
+          return <ProjectItem 
+            name={name} 
+            about={about}
+            technologies={technologies}
+            key={project.id}
+            />
+        })}
+      </div>
     </div>
   );
 }
